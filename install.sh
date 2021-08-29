4bedfed5f69a70507a14532c4bbd10aff391a9ad
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-29 21:29:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /usr/bin
-wget -q "https://raw.githubusercontent.com/zerovpn1/vpn/main/zero"
-wget -q -O menu "https://raw.githubusercontent.com/zerovpn1/vpn/main/menu.sh"
-wget -q -O ssh "https://raw.githubusercontent.com/zerovpn1/vpn/main/ssh.sh"
-wget -q -O wgr "https://raw.githubusercontent.com/zerovpn1/vpn/main/wgr.sh"
-wget -q -O l2tp "https://raw.githubusercontent.com/zerovpn1/vpn/main/l2tp"
-wget -q -O sstpp "https://raw.githubusercontent.com/zerovpn1/vpn/main/sstpp.sh"
-wget -q -O ssssr "https://raw.githubusercontent.com/zerovpn1/vpn/main/ssssr.sh"
-wget -q -O v2raay "https://raw.githubusercontent.com/zerovpn1/vpn/main/v2raay.sh"
-wget -q -O vleess "https://raw.githubusercontent.com/zerovpn1/vpn/main/vleess.sh"
-wget -q -O trojaan "https://raw.githubusercontent.com/zerovpn1/vpn/main/trojaan.sh"
+wget -q "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/zero"
+wget -q -O menu "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/menu.sh"
+wget -q -O ssh "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssh.sh"
+wget -q -O wgr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/wgr.sh"
+wget -q -O l2tp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/l2tp"
+wget -q -O sstpp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/sstpp.sh"
+wget -q -O ssssr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssssr.sh"
+wget -q -O v2raay "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/v2raay.sh"
+wget -q -O vleess "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/vleess.sh"
+wget -q -O trojaan "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/trojaan.sh"
 chmod +x menu && sed -i -e 's/\r$//' menu
 chmod +x ssh &&  sed -i -e 's/\r$//' ssh
 chmod +x wgr && sed -i -e 's/\r$//' wgr
