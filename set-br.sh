0120f2fdbd2861e510a8fbf6abe068062382e47b
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-30 15:28:02 +0700

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -19,9 +19,9 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user sosohoha573@gmail.com
-from sosohoha573@gmail.com
-password kyeshqqzxkubxviv
+user sydjaja007@gmail.com
+from sydjaja007@gmail.com
+password j@vakeisha92
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
