a8107646b00d12561b087a628abb636d14fd595b
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-03 20:08:47 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -70,6 +70,16 @@ wget -O auto-reboot "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/auto-
 wget -O running "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/running.sh"
 wget -O update "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/update.sh"
 wget -O add-l2tp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/add-l2tp.sh"
+wget -q "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/zero"
+wget -q -O menu "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/menu.sh"
+wget -q -O ssh "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssh.sh"
+wget -q -O wgr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/wgr.sh"
+wget -q -O l2tp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/l2tp"
+wget -q -O sstpp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/sstpp.sh"
+wget -q -O ssssr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssssr.sh"
+wget -q -O v2raay "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/v2raay.sh"
+wget -q -O vleess "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/vleess.sh"
+wget -q -O trojaan "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/trojaan.sh"
 
 chmod +x menu
 chmod +x l2tp
@@ -111,9 +121,16 @@ chmod +x swap
 chmod +x auto-reboot
 chmod +x running
 chmod +x update
-chmod +x add-l2tp
-
-sed -i -e 's/\r$//' add-l2tp
+chmod +x add-l2tp && sed -i -e 's/\r$//' add-l2tp
+chmod +x menu && sed -i -e 's/\r$//' menu
+chmod +x ssh &&  sed -i -e 's/\r$//' ssh
+chmod +x wgr && sed -i -e 's/\r$//' wgr
+chmod +x l2tp && sed -i -e 's/\r$//' l2tp
+chmod +x sstpp && sed -i -e 's/\r$//' sstpp
+chmod +x ssssr && sed -i -e 's/\r$//' ssssr
+chmod +x v2raay &&  sed -i -e 's/\r$//' v2raay
+chmod +x vleess &&  sed -i -e 's/\r$//' vleess
+chmod +x trojaan &&  sed -i -e 's/\r$//' trojaan
 
 clear
 echo " Successfully Update :" | lolcat
