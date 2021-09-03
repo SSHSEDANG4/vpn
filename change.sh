89908a39531f53a2d7b61014dde8aace2d39784f
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-03 18:47:26 +0700

    Add files via upload

--- change.sh
+++ change.sh
@@ -2,30 +2,21 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/vps-ip/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
+echo -e "Name : Change Port All Service"
+echo -e "======================================" | lolcat
 echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "     [1]  Change Port Stunnel4"
-echo -e "     [2]  Change Port OpenVPN"
-echo -e "     [3]  Change Port Wireguard"
-echo -e "     [4]  Change Port Vmess"
-echo -e "     [5]  Change Port Vless"
-echo -e "     [6]  Change Port Trojan"
-echo -e "     [7]  Change Port Squid"
-echo -e "     [8]  Change Port SSTP"
-echo -e "     [x]  Exit"
-echo -e "======================================"
+echo -e "     [1]  Change Port Stunnel4" | lolcat
+echo -e "     [2]  Change Port OpenVPN" | lolcat
+echo -e "     [3]  Change Port Wireguard" | lolcat
+echo -e "     [4]  Change Port Vmess" | lolcat
+echo -e "     [5]  Change Port Vless" | lolcat
+echo -e "     [6]  Change Port Trojan" | lolcat
+echo -e "     [7]  Change Port Squid" | lolcat
+echo -e "     [8]  Change Port SSTP" | lolcat
+echo -e "     [9]  Change Port Trojan Go [CLOSED]" | lolcat
+echo -e "     [x]  Exit"| lolcat
+echo -e "======================================" | lolcat
 echo -e ""
 read -p "     Select From Options [1-8 or x] :  " port
 echo -e ""
@@ -53,6 +44,9 @@ port-squid
 ;;
 8)
 port-sstp
+;;
+8)
+port-trgo
 ;;
 x)
 clear
