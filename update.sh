a737b2f2bae3d76674df0488752533d14e16470d
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-03 20:13:12 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -75,11 +75,6 @@ wget -q -O menu "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/menu.sh"
 wget -q -O ssh "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssh.sh"
 wget -q -O wgr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/wgr.sh"
 wget -q -O l2tp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/l2tp"
-wget -q -O sstpp "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/sstpp.sh"
-wget -q -O ssssr "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/ssssr.sh"
-wget -q -O v2raay "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/v2raay.sh"
-wget -q -O vleess "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/vleess.sh"
-wget -q -O trojaan "https://raw.githubusercontent.com/SSHSEDANG4/vpn/main/trojaan.sh"
 
 chmod +x menu
 chmod +x l2tp
@@ -122,15 +117,6 @@ chmod +x auto-reboot
 chmod +x running
 chmod +x update
 chmod +x add-l2tp && sed -i -e 's/\r$//' add-l2tp
-chmod +x menu && sed -i -e 's/\r$//' menu
-chmod +x ssh &&  sed -i -e 's/\r$//' ssh
-chmod +x wgr && sed -i -e 's/\r$//' wgr
-chmod +x l2tp && sed -i -e 's/\r$//' l2tp
-chmod +x sstpp && sed -i -e 's/\r$//' sstpp
-chmod +x ssssr && sed -i -e 's/\r$//' ssssr
-chmod +x v2raay &&  sed -i -e 's/\r$//' v2raay
-chmod +x vleess &&  sed -i -e 's/\r$//' vleess
-chmod +x trojaan &&  sed -i -e 's/\r$//' trojaan
 
 clear
 echo " Successfully Update :" | lolcat
