d2c90221ac5ef6678fb6566108a102731a116f23
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-05 21:07:37 +0700

    Add files via upload

---
+++ port-ws-ssl.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+ssl="$(cat /etc/stunnel/stunnel.conf | grep -i accept | head -n 4 | cut -d= -f2 | sed 's/ //g' | tr '\n' ' ' | awk '{print $4}')"
+echo -e "======================================" | lolcat
+echo -e "Name : Change Port Websocket SSL"
+echo -e ""
+echo -e "     [1]  Change Port $ssl"
+echo -e "     [x]  Exit"
+echo -e "======================================" | lolcat
+echo -e ""
+read -p "     Select From Options [1 or x] :  " prot
+echo -e ""
+case $prot in
+1)
+read -p "New Port Websocket SSL : " stl
+if [ -z $stl ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $stl)
+if [[ -z $cek ]]; then
+sed -i "s/$ssl/$stl/g" /etc/stunnel/stunnel.conf
+sed -i "s/   - Websocket SSL           : $ssl/   - Websocket SSL           : $stl, $ssl/g" /root/log-install.txt
+/etc/init.d/stunnel4 restart > /dev/null
+echo -e "\e[032;1mPort $stl modified successfully\e[0m"
+else
+echo "Port $stl is used"
+fi
+;;
+x)
+exit
+menu
+;;
+*)
+echo "Please enter an correct number" | lolcat
+;;
+esac
