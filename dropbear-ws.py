e57c033c9367c31b9d0986a17ac8a804dd1cc46c
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-06 23:16:57 +0700

    Add files via upload

--- dropbear-ws.py
+++ dropbear-ws.py
@@ -3,9 +3,11 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
-
-# Pass
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 2095  
+#Pass
 PASS = ''
 
 # CONST
