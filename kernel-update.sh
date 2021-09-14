6e5354a103236e0a8de4fd14f2dd1e10fc420cb7
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-14 17:29:04 +0700

    Update kernel-update.sh

--- kernel-update.sh
+++ kernel-update.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 clear
 source /etc/os-release
 OS=$ID
-if [ $OS = 'ubuntu' ]; then
+if [ $OS = "ubuntu" ]; then
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 install ubuntu-mainline-kernel.sh /usr/local/bin/
 rm -f ubuntu-mainline-kernel.sh
@@ -17,7 +17,7 @@ exit 0
 else
 printf "y" | ubuntu-mainline-kernel.sh -i
 rm -f /usr/bin/ubuntu-mainline-kernel.sh
-elif [ $OS = 'debian']; then
+elif [ $OS = "debian"]; then
 ver=(`apt-cache search linux-image | grep "^linux-image" | cut -d'-' -f 3-4 |tail -n1`)
 now=$(uname -r | cut -d "-" -f 1-2)
 if [ $ver == $now ]; then
