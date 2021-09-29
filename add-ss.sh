16bd1959f94bd61bf2373abae916078122dedd85
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-29 16:24:21 +0700

    Update add-ss.sh

--- add-ss.sh
+++ add-ss.sh
@@ -1,4 +1,107 @@
 #!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/sshsedang/main/kota/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+IP=$(wget -qO- ifconfig.me/ip);
+lastport1=$(grep "port_tls" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+lastport2=$(grep "port_http" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
+if [[ $lastport1 == '' ]]; then
+tls=2443
+else
+tls="$((lastport1+1))"
+fi
+if [[ $lastport2 == '' ]]; then
+http=3443
+else
+http="$((lastport2+1))"
+fi
+echo ""
+echo "Name : Create Shadowsocks Account"
 
-God="BlessYou";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=TF;oTX=dD;Yox=Qa;wQx=mb;Kqb=Zy;xWU=twB;gTp=auw;hwZ=Ejp;tHg=iQZ;ojo=shb;jhw=FjF;IDL=klV;wOA=yFT;PZL=XfL;DRJ=DiE;bmJ=FCa;XGN=dOT;QCK=dkY;SOb=WJE;RJx=uAv;czJ=dts;VGB=oCy;TyO=xGj;yHU=bpq;Nfb=Wct;doQ=Ibf;XCL=rax;ZNH=Hhf;Eoh=yCU;ghd=hut;ooz=VKY;qMf=PrY;Ecf=Lke;BbJ=jie;QOI=lOT;oi=QkU;yM=iDO;rT=twI;wa=BEk;Lf=SGl;Sx=Kri;yL=RiF;Xf=iZT;iv=fpC;fW=nki;qX=FrL;ba=OUY;czP=Hdm;ecN=WKt;QMW=BnB;Nsq=jdC;nof=OZt;bhx=qNV;vVN=xaU;smp=Wta;ceY=GDh;AIB=juO;miA=Cso;zgz=Hka;Kea=YMq;hUp=SWY;zMP=ozk;WGK=JEt;pkF=jjF;FCq=pFs;ZrQ=VcQ;ZjS=do;BTP=Su;sly=xM;aZl=vF;iJn=bo;QCl=HB;SCO=xo;fUq=Jo;Epq=zc;wxq=Ev;ziJ=Gx;Kzo=JF;PDO=pJ;qhq=yp;OYk=rj;OGy=Cc;hSK=xR;lnD=um;UXi=pl;xBF=xP;qOD=iX;RVy=nl;mqe=JK;rQt=CA;qJD=FR;sPp=XZ;moh=DC;Fsj=GWh;NTx=Jnx;dwG=ncr;Ugd=mFV;ZTt=WjI;mFG=lms;YGK=bPI;UKs=ibv;hka=JFE;zpQ=Gxt;VlF=Jxk;AEJ=cuk;SUZ=YiV;Wfq=NhZ;xLp=ray;QmE=mqM;fkp=tHl;Sek=XvT;qbj=EKu;RfA=xSe;fPQ=Oll;cif=jtg;GYE=yei;Prw=RTD;sMB=pEL;sxA=APb;aRs=HgZ;SXm=VSI;lZA=UKc;YMw=Lct;bwO=XQI;OxQ=COX;bPh=KqP;GVr=hRx;eoA=eID;AZK=fFj;PGC=XPy;ieV=gly;AUO=yit;GjW=jaZ;tBB=HSO;vdV=efl;HiW=VHx;JaQ=RCo;cGr=fsk;eib=jty;TPK=rlb;dhM=oDt;tCL=muK;gAg=xQq;xWl=eMt;AtB=mbQ;UzS=lee;xjU=CPZ;qLS=iIg;dQw=NHN;Dum=qAc;aei=Whw;afz=OcR;NWZ=Txs;gzI=HsZ;YmH=IIm;kZc=Ppd;qnZ=vjw;sQw=BmF;PAB=rGe;gah=aHu;XfD=uJJ;LmJ=PUK;jCz=XpN;gTg=hRF;cpS=fhx;wFI=fih;pHM=aTH;GIm=Jtb;XDy=XGY;zMs=kPd;VtP=pbP;ALj=acU;HEZ=Tre;EPr=vOf;eSC=AIk;TXH=szR;nNa=bGD;lQt=Qod;Qvm=HGI;tkS=IOY;qmZ=tDf;xxx=xOS;pFg=XCd;aVR=xGh;ooR=AHX;JVj=laY;TvM=kEx;SYU=hol;ddj=khS;aPP=oiI;Edo="
-";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt="l";Yxgs="";qwt="Fu";ieC=HO;XcH=Rv;Ugs=LC;vD=xp;fy=oW;Zl=LhH;oP=Jm;sDA=kpY;GHn=TB;wzk=Da;oiO=Ei;NaM=hIf;fC=vjz;kT=rz;sV=bZz;CL=ARL;Wc=dA;gZG=tGI;vpk=cM;Aao=tgy;sU=ld;LZ=Pr;BE=wCX;iN=fQ;RCe=Dm;Rc=Fl;Njo=OMq;gtb=SKa;Wf=jMB;Dm=wHr;Nx=Wn;ANT=jAK;oFy=Bnc;IjZ=Xj;DQa=oK;ewZ=Vm;QPf=lz;Cwg=aS;KEM=jS;fkE=hM;nLw=ym;FWv=BC;aDC=Lw;tW=Rgn;kw=Hgf;tQ=eSh;Fi=iGb;Or=snM;ZG=xqx;xC=WJr;Bf=haC;pT=THr;By=jbu;tm=Hcy;eT=nTC;UT=KCa;dk=Pxy;vG=TvX;HX=MMB;Ow=ZoT;UJ=LvE;rx=oMF;EB=VZx;OC=IfU;dv=lTx;Nk=TNq;Ej=bKu;dl=ZZi;yzL=bJ;EiG=LJ;iqg=gK;fbd=vg;OPl=Ei;tnI=YS;yBV=DA;yxh=Ks;Nic=Ih;wMq=tD;iDs=AQ;Nhh=zy;JWe=Ry;gC=AnI;qWN=uq;hIc=OAL;UOm=Nq;Xj=ax;GW=utn;Fmc=CaI;zeK=eT;hI=sl;Ae=RK;XV=doy;Dpq=Eg;Mmr=MZg;CXd=Bao;RND=WUC;CGm=SRK;DJh=LZw;ym=Ah;xzv=IX;FYi=YUX;v6T=" ";RzE="Z1kCn0GMbVGXn0zQOpwJtJzM7AzWlx1J94WZlJ3ZKcSbxMzOxsVZcdSPkVmcKg2chJ2LulmYvEyI 94USalkCiMFUWByZul2ajVGaDJCIvh2YlpwOpAXavUWbucWam52bjZWag0yTx1CI0V2Z3hCJ9AVS zh2cz9CNH5UQEV0UIN1Uv02bj5CduVGdu92YyV2c1JWdoRXan5ydhJ3LvozcwRHdoBCbyV3YggCJ dBiTJpVSkASPgAVSZ1EJgsFImlmCpACUJlVTkACclJ3ZgwHIzBndwl2LhR3br9ibpFWbvcmbhRWZ lNHblpgI9NkT7RiLu4CZlRHclN2YBBibvl2czlWbyVGU95WZlJ3Z7RiIgUWLg8GajVmCuVGa0ByO gI3bGBSes52TiAyboNWZKsjI9NkT7RSIkVWauVGRg42bpN3cp1mclBVfkVmc7RiIgUWLg8GajVmC l1mLnlmZu92YmlGIt8UctACdld2doQSPQlkCyFWZsNmCpZmCwACdphXZKIycyV2cVBSb1lWblJHU vYXZilGbtM3aj92c39GZhh2cvMGdl9CIiMHb09Fdy9GciACclJ3ZoQSPxQncvBHdzFGbKsTKwl2L wVmcnhCJ9IDdy9Gc0NXYspQKn0nMkACdulmcwt3Jgs2dhBCfgEjbtACbpFGdgwHIm52bj5ib1tWY gEjbtACbpFGdgwHIm52bj5ib1tWYvYXZilGbtM3aj92c39GZhh2cvMGdl9CIiAHd0h2X0J3bwJCI y0zcsRnCuVGa0ByOd1FIncCI90DIxQncvBHdzFGbkAyWbBiZppQKn0nMkACdulmcwt3Jgs2dhBCf ncCI90DIyQncvBHdzFGbkAyWbBiZppQampgIpkSMrEDdy9Gc0NXYshCKkISPzxGdKU2csVmCzQDN g8GajVmCpZmCikSKxsiM0J3bwR3chxGKoQiI9AHd0hmClNHblpwM0QzM9AHd0hmCuVGa0ByOd1FI tAjWtEketE2WeBif9AiclNXdkAyWbBCbpRnb1pgCiQmcvd3czFGcg4WYrtWdzFWTiAyboNWZKIiI lBCIgACIgACIgACIgACIgAiCvRGI70VXgcCMnASP9ASfTR1UJhVRfRlTFlETDtHJgYiJgQyKd9VO 90TP90TP90TPiAyboNWZJkgCiQnb192YjFEIzt2YvN3dvRWYoNFIlRXYlJ3QgoDIl1WYOJCIvh2Y 1BSZtAiIgoDZy92dzNXYQJCIwJXLgQWYlJXCJoAdhNGbvxGI8BiI90TP90TP90TP90TP90TP90TP lJWas1ycrN2bzd3bkFGaz9yY0V2LgIXZzVHJgcXLgAXZydGKk0zUUNVSYV0XU5URJx0QJkgCyV2c 70VXgcSMnASP9ASfTR1UJhVRfRlTFlETDtHJgs1WgYWaJkgCKkCbtAyY3BCfgYmbvNmLuV3ah9id tFmbgQWZpZWajVGczBSZoRHIoRXa3BCduVWasNGIBJCIvh2YllQCJogIiAyboNWZJkQCK4WZoRHI 4VWCJkgCi4SZtFmbgIXZoR3buFGIlN3bvh2YgU2chVGbwBCLkVGdhVmcjBSekFWZyxWYgMXY3BSZ 9AHelpgZpR3ahF2ch1GIiAiOpkmchhGKgQWZylGc4VkIgAXLgQWYlJnCl52bklgCpZWCJoQMgQXa 39GZhh2cvMGdl9CI+ACdhNmCgJCZl0Sbl0SWlIyKgIyc5FGZgYWa0tWYhNXYtRiIgQWLgUGdhRGY uAjLw4CMiojIyVmdyV2ciACIgAiCgACI7pAROVEP842bzpmLzxGdtIXZzVHJvYXZilGbtM3aj92c iACIgAiCsIiclNXdkIiOiQmcvd3czFGciACIgAiCsMHb0RiOiQncvB3XyVmdyV2ciACIgAiCsICM 6IiblB3bfR3chZmIgACIgoALiImZj1iN1ITLzVWYiojIk9Ga0VWbiACIgAiCsAjN6ICd19WZtlGd gAiCsICOugjL44COiojIyVmdyV2cl1WYuJCIgACIKwSZ1JHd6ISehxWZk91buJCIgACIKwSZ1JHd iACIgAiCsIiclZnclNXLzZmYvJiOi4WanVHbwJCIgACIKwiIwRWdfRmbh9FcjRnI6ISZk9WbiACI lJWas1ycrN2bzd3bkFGaz9yY0V2Lg4DI0F2YKQkTFpQfKIycsRXPzZmYvJiOiMHdw92Xul2Z1xGc zJCIgACIKwiIw4CMuAjLwIiOiIXZ2JXZzJCIgACIKsnCE5URtwDPg42bzpmLwRHdo1iclNXdk8id 6ICd19WZtlGdiACIgAiCsIiclNXdkIiOiQmcvd3czFGciACIgAiCsAHd0hGJ6ICdy9GcfJXZ2JXZ gACIKwSZ1JHd6IiblB3bfR3chZmIgACIgoALiImZj1iN1ITLzVWYiojIk9Ga0VWbiACIgAiCsAjN 6ISZk9WbiACIgAiCsICOugjL44COiojIyVmdyV2cl1WYuJCIgACIKwSZ1JHd6ISehxWZk91buJCI w92Xul2Z1xGciACIgAiCsIiclZnclNXLzZmYvJiOi4WanVHbwJCIgACIKwiIwRWdfRmbh9FcjRnI zVHJvYXZilGbtM3aj92c39GZhh2cvMGdl9CI4tCIk9WboNmCE5URK0nCiAHd0hWPzZmYvJiOiMHd zpmLwRHdo1iclNXdk8idlJWas1ycrN2bzd3bkFGaz9yY0V2Lgg3KgQ2bth2YK42bzpmLzxGdtIXZ lNnLzxGdtIXZzVHJAJXZ2JXZz1idlJWas1ycrN2bzd3bkFGazBCdyFGdzBCb0NWblR3c5NnCK42b zxGdtIXZzVHJAJXZ2JXZz1idlJWas1ycrN2bzd3bkFGazBSZsJWYuVGIsR3YtVGdzl3cKU2YpZnc o1iclNXdkAkclZnclNXL2VmYpxWLzt2YvN3dvRWYoNHI0JXY0NHIsR3YtVGdzl3cKU2YpZnclNnL zVHJAJXZ2JXZz1idlJWas1ycrN2bzd3bkFGazBSZsJWYuVGIsR3YtVGdzl3cKU2YpZnclNnLwRHd 9BVS7RCQ9JXZzV3ekojYmNWL2UjMtMXZhJCIu1CIvh2YlhCJ9EDctRnClNWa2JXZz5Cc0RHatIXZ A1nclNXd7RiOiZ2YtYTNy0yclFmIg4WLg8GajVGKk0jMw1GdKkCM31CI0YTZzFmYgwHIiMHb0RiO v1jbpdWdsB3P9FDctR3ek8yL6M3ci0TMzN3aulGbKkCM31CI0YTZzFmYgwHIiAHd0hGJ60HUJtHJ tR3ek8yL6M3ci0jMzN3aulGbKISbvNmLn5Wai1Ddz9GatMnZi92OzxGd9MnZi92OsF2YvxWLzZmY g8GajVmCi02bj5yZulmY9Q3cvhWLzZmYvtDc0RHa9MnZi92OsF2YvxWLzZmYv1jbpdWdsB3P9JDc vMGdl9iI+4jIwRHdoRCIwRHdo9Fdy9GcKMHb0RCIzxGdfRncvBnCwhXZkAiclNXdkAyIjMiIgUWL JogchVGbjpAdyFGdzVmcg42byNGIlNWa2JXZzpgIm52bj5ib1tWYvYXZilGbtM3aj92c39GZhh2c 90TP90TPiASZtAyboNWZJogIzt2YvN3dvRWYoNFI6ASZtFmTiASZtAyboNWZJogIiASZtAyboNWZ gACIgACIgACdz9GSvAVSiASZtAyboNWZJoAdhNGbvxGI8BiI90TP90TP90TP90TP90TP90TP90TP C9EI0J3bQJCIl1CIvh2YllgCiMHb0RCI6ACITxEVgMlRC9EI0J3bQJCIl1CIvh2YllgCiAVSkAiO g8GajVWCKIiclNXdkAiOgACIgACIgQmcvd3czFGUiASZtAyboNWZJogIwRHdoRCI6ACUURFSgMlR gACIu9EIkVmcpBHeFJCIl1CIvh2YllgCiImZj1iN1ITLzVWYgoDIgACIgACIgACZvhGdl1kIgUWL 0F2Ys9GbgwHIi0TP90TP90TP90TP90TP90TP90TP90TP90TP9ICIl1CIvh2YllgCiAHelRCI6ACI 90TP90TP90TPiASZtAyboNWZJogIxM3cr5WasRCI6ACITxEVgMlRC9EIr5WaMJCIl1CIvh2YllgC kAiOgAFVUhEITZkQPByaulGTiASZtAyboNWZJoAdhNGbvxGI8BiI90TP90TP90TP90TP90TP90TP KQXYjx2bsBCfgISP90TP90TP90TP90TP90TP90TP90TP90TP90jIgUWLg8GajVWCKIiMzN3aulGb =ogILJ1TXRVROByROFERFNFIIN1UgknQgQHcpJ3YTJCIl1CIvh2YlBCIgACIgACI";Mll="ch";HTv=Yyu;cbY=cId;FwM=nUK;Neo=URi;Jrd=ZsO;ZzZ=gsI;AGg=Krt;KoC=Usx;ftb=Vzm;kuT=EVS;AUX=NGv;Aow=suL;uBc=hlq;OqT=ApZ;kSe=ZTL;aFt=FKz;yqF=lEj;ZII=uml;BwC=JPx;gWH=eyv;wWA=hWw;unn=Brn;hxs=DiR;nPo=iOr;Uuo=lUG;zxW=Nkd;MAi=aWZ;XcA=xIq;sZL=MLV;rwV=lNF;GYU=UPS;WeS=kTk;Aym=btt;rGW=IDI;Uhx=DhE;Kut=fsH;eGn=UyX;VNh=YtK;QWo=gEO;Byf=nvi;Ezb=oBV;uWP=JEl;Vsp=Lgc;EAF=Byk;PSz=HCg;pyP=bQL;Uxq=ehM;WdA=Uij;pWM=bsv;oXm=BKe;mtM=qkr;XEB=pua;XzD=SPa;Kng=JSY;bFL=Zud;xbc=GSY;neu=TQJ;Ttj=EdI;ZCG=HXS;NSP=aRL;UPz=CwR;edR=xrL;jQC=PQv;ESa=fXd;eKc=KZo;WEy=DoO;fQg=tcd;FCk=GMk;YgE=Nzd;FAs=AFH;gRJ=wZL;ffM=Jsb;Ixf=RQY;Gmk=ugg;GRa=CTs;UiO=Yer;etb=mgM;fex=MqF;Nll=vPC;KWd=yXb;Muu=Mzy;wHZ=jjp;SmF=EZc;ZqQ=lNR;CuE=Afy;weE=KKV;ggU=fdX;wbE=YVE;TEj=yAz;and=qKr;MVm=jME;jUo=PRn;lUm=BLo;IUT=oDE;BDY=lkB;oQp=FKc;YEa=bpY;qOb=Izx;MfR=ODo;JmY=Kia;weH=FVf;URv=vSO;Jdp=BJH;opM=fhP;NUx=Qpe;iDY=ZNQ;lAj=vuK;imh=QaU;Cdp=mgw;wAJ=IMc;ANx=UQv;CkG=nsS;tuB=Lfj;KFb=ACa;Znn=SPN;Izy=AKY;MCI=uEV;Vas=kJi;OVF=xhN;ocJ=mis;CCq=zoG;Uun=Bsu;Vet=cPB;PMj=gXU;Svx=qVt;ado=LSJ;zGv=aIv;jyW=gEB;KLw=yan;nqU=foW;mPS=Zhi;oRH=HzR;aTq=iAA;LFG=yTk;LhE=dvZ;Gmd=aBt;lDM=FUf;KkS=LUv;ODo=ifH;LIW=oRx;kbu=cPC;WgK=RfX;NoQ=OpS;VWA=MwS;bPe=cOh;drL=JXi;tuj=aeR;nlI=RgR;qog=uHu;zLA=FCa;INo=pXp;PaQ=yJV;kPu=dmu;EgN=Wek;MXp=PIc;YEF=xAk;Zvz=EGN;Feh=yij;sv=TmN;Gc=WAR;kw=XRs;dT=fhJ;ed=WAu;rq=OFi;ak=auS;zU=gWJ;QB=PyR;Ci=HYT;DR=HGU;nEl=izK;Yon=JKg;aRR=osG;BLG=nGa;fad=iUJ;FeM=Oph;NIf=nUq;AdK=IHM;pMr=KRX;GJr=sor;Yzr=JJM;zCc=jhW;iac=Znx;lPF=qCm;GHi="o";Re="-";LcV=" ";H2o="t";O2h="r";r="ev";Gi="ba";x="s";sfT="d";x64="4";CTh=Vu;sh=dMK;Pl=GnF;MQ=HQe;dG=hZi;Tv=TuJ;BP=PPf;nh=Edv;pY=vGA;io=bPH;Rr=tdy;MF=jZJ;my=zmB;Yd=mtW;XA=kqf;VR=qss;BP=enZ;re=dnQ;os=Xua;kA=hzB;aE=JKy;vG=pcw;SO=gBc;FO=XMq;kK=ZHi;DwX=sS;mNb=im;nv=Cn;yNI=Hb;Yt=FQ;Pl=PU;ko=DJA;UX=tv;XD=UYM;uz=hIa;YVD=cJM;Zde=fi;IF=hLs;JCM=Dg;Fdw=JL;ieQ=FAu;ZMh=TQB;qHK=EM;wVK=bxY;nMR=mC;UL=Lep;wJ=eM;yc=dch;rIX=Vb;bBO=eT;Zpx=Iz;ang=Ln;AYe=Qy;UiQ=IU;usu=Xy;rkj=Ce;eVp=OY;aFw=wR;uNw=cJ;DQb=Tn;ZLx=ec;ipH=hk;pcK=rd;lCz=Ce;jxG=nx;Gkd=Pl;yFz=zk;vow=Qm;QBs=gz;dXe=sQ;dJU=vr;urX=mj;IF=qGI;UoJ=hD;x=$(eval $eLoP$Mll$GHi$v6T$Re$eLoP$v6T$RzE$v6T|$v6T$H2o$O2h$LcV"$LcV"$LcV"$Edo"$LcV|$v6T$O2h$r$v6T|$v6T$Gi$x$eLoP$u18$x64$v6T$Re$sfT);eval "$x";GMZ=snx;llB=diL;pxn=skL;Syg=GFT;RaJ=zXK;ZKL=BSQ;XCH=tmb;eyL=TOc;Ime=EZW;TFM=mN;tUA=Ab;fnp=ch;FNp=pJ;JRk=Az;YXw=hu;Qde=tk;zmI=lH;MdB=kG;cRe=zm;ygE=Og;MrN=Wn;Rwc=UL;KRw=NX;AmN=nu;lUf=HN;HVY=od;pqW=bz;hzm=tv;vAz=ql;fVA=KK;WXS=HX;PRd=CDX;lwM=wEt;ZcX=rAB;Yta=Syw;hvo=gTx;mtU=YbQ;Ilo=unK;Ava=XiM;mET=pHp;fFk=Pno;XBK=taz;dtp=zbA;bMu=zKL;FPC=PwO;xXA=dwc;Alj=ylC;JQq=JPx;MUH=FcT;cvl=azu;HPj=sSJ;dsl=XvS;Hoq=nKA;AZD=sfK;JFc=NZK;eEE=DrT;ZhW=fMr;VBT=enR;poD=hwH;lLU=qkd;yTa=OEZ;ZpV=WQP;Jeg=Mwj;YGH=HdD;mKz=pLU;PNn=ako;scF=rtk;QTH=sHP;Eto=FIR;BlM=WAX;UHF=WBV;Fud=mEb;sGh=Zzy;lPW=SOK;mcu=ZDf;QdD=YZM;HcF=LzS;GNn=Hyn;cql=ioJ;LKm=DNv;TAH=FaF;VYt=ERc;vhF=xvU;tNq=Uzi;JaE=bgV;bpG=opJ;CsA=UQX;pGD=TXG;AZk=bTy;bId=bNE;yql=fVT;Xcy=BmA;UHz=jRf;UNz=KZk;PXl=Vsy;Stv=Sed;cyB=zLX;gGz=JJP;IyT=xSZ;Zdj=nVJ;yQZ=vVc;sHL=Vfg;Zro=zth;XPf=sFS;nQQ=dnd;SNh=eWO;fOZ=Xjc;fgB=rQS;UjE=cpS;xYk=qWB;koB=ZFE;oQQ=hSL;ufm=zwQ;mzC=UfF;gQl=SLT;wwc=Jcb;tDN=SNI;ZFg=atH;UlF=ajb;DaK=zqX;KFL=uPP;mjy=zbB;ofO=nbM;vfl=vdB;SDM=yQQ;AfY=mzE;IHi=AeT;oVK=WsK;XhO=hRC;pOA=ScU;bCh=FOX;rgF=qzX;zyy=hXg;utq=MGT;Lkd=CTs;Vlb=XYZ;Ziu=aFC;dgF=qOn;Bpe=yss;Xoc=NXh;dGV=AjA;Zux=APj;diU=LZO;Vzp=lBl;xDW=otu;wxh=LmE;jsJ=xnu;AuO=OCw;iZA=dLf;pmj=lwe;RIo=tLO;TcY=glu;EVQ=Bxb;hkK=RTP;GcY=FoO;TDT=VhE;iZG=wyh;FNH=Bmj;BJh=mAj;SCs=hOS;kkj=Dgb;une=TET;DWN=wEf;JZr=EEn;eev=vSC;Gcg=IFa;jkQ=FDn;uiO=zBO;JIM=vwW;GtR=lZX;WoV=mWR;bZm=gDj;XgT=spD;dfG=pHI;IjF=XxS;pWp=hMH;Ul=fIj;dD=mht;yw=cOO;Vw=kik;Zo=RGl;XT=hQB;CB=Ofi;mM=Dun;cN=Uyp;rL=yIY;eD=qYt;jB=nnI;Wf=not;Yv=Rqc;Yu=OgM;xZ=FzE;ec=jKM;gB=qoM;rZ=lDg;HY=BTw;LB=TJn;Tk=qDl;HY=nSN;kp=iUS;ThankYou="HaveNiceDay";
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+                echo "==========================="
+                echo "Masukkan password"
+		echo "===========================" | lolcat
+		read -rp "Password: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/shadowsocks-libev/akun.conf | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+read -p "Expired (hari): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+cat > /etc/shadowsocks-libev/$user-tls.json<<END
+{   
+    "server":"0.0.0.0",
+    "server_port":$tls,
+    "password":"$user",
+    "timeout":60,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=tls"
+}
+END
+cat > /etc/shadowsocks-libev/$user-http.json <<-END
+{
+    "server":"0.0.0.0",
+    "server_port":$http,
+    "password":"$user",
+    "timeout":60,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "no_delay":true,
+    "nameserver":"8.8.8.8",
+    "mode":"tcp_and_udp",
+    "plugin":"obfs-server",
+    "plugin_opts":"obfs=http"
+}
+END
+chmod +x /etc/shadowsocks-libev/$user-tls.json
+chmod +x /etc/shadowsocks-libev/$user-http.json
+
+systemctl start shadowsocks-libev-server@$user-tls.service
+systemctl enable shadowsocks-libev-server@$user-tls.service
+systemctl start shadowsocks-libev-server@$user-http.service
+systemctl enable shadowsocks-libev-server@$user-http.service
+tmp1=$(echo -n "aes-256-cfb:${user}@${IP}:$tls" | base64 -w0)
+tmp2=$(echo -n "aes-256-cfb:${user}@${IP}:$http" | base64 -w0)
+linkss1="ss://${tmp1}?plugin=obfs-local;obfs=tls;obfs-host=bing.com"
+linkss2="ss://${tmp2}?plugin=obfs-local;obfs=http;obfs-host=bing.com"
+echo -e "### $user $exp
+port_tls $tls
+port_http $http">>"/etc/shadowsocks-libev/akun.conf"
+service cron restart
+clear
+	echo -e ""
+	echo -e "Name : Shadowsocks"
+	echo -e "===========================" | lolcat
+	echo -e "IP/Host        : $IP"
+	echo -e "Port OBFS TLS  : $tls"
+	echo -e "Port OBFS HTTP : $http"
+	echo -e "Password       : $user"
+	echo -e "Method         : aes-256-cfb"
+	echo -e "Expired On     : $exp"
+	echo -e "===========================" | lolcat
+	echo -e "Link OBFS TLS  : $linkss1"
+	echo -e "===========================" | lolcat
+	echo -e "Link OBFS HTTP : $linkss2"
+	echo -e "===========================" | lolcat
+        echo -e "Script By SSH SEDANG NETWORK"
